74693bc4370382bf91bf0ce6eb493c7e18cf75fd
Zaephus14 <rozendaalmarc@gmail.com>
2022-11-06 15:54:53 +0100

    Ready to build

--- Show_Me/Assets/Scripts/GameManager.cs
+++ Show_Me/Assets/Scripts/GameManager.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject endMenu;
     [SerializeField] private GameObject game;
+
+    public void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            PauseGame();
+        }
+    }
 
     public void StartGame() {
         mainMenu.SetActive(false);
@@ -14,6 +23,25 @@ public class GameManager : MonoBehaviour {
 
     public void ExitGame() {
         Application.Quit();
+    }
+
+    public void EndGame() {
+        game.SetActive(false);
+        endMenu.SetActive(true);
+    }
+
+    public void NewGame() {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void PauseGame() {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame() {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
     }
 
 }
